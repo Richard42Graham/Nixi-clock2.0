493ce797660d70b78775421c02314eaeeaef8e50
Richard42Graham <richard.graham1@myport.ac.uk>
2018-05-06 21:27:47 +0200

    Updated: The compile script to work with the new structure

--- compile.sh
+++ compile.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
-gcc clock.c -lm -lwiringPi $(/usr/bin/python2.7-config --ldflags)
+#!/bin/bash
+echo Compiling...
+gcc -o main.out main.c speedtest.c initialize.c clock.c -lm -lwiringPi $(/usr/bin/python2.7-config --ldflags)
 
- ./a.out
+if [ $? -eq 0 ]
+then
+	echo Runs...
+	./main.out
+else
+	echo "Compile failed!";
+fi
--- compile2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcc -o main main.c speedtest.c initialize.c clock.c -lm -lwiringPi $(/usr/bin/python2.7-config --ldflags)
-
-./main
